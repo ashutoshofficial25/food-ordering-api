3491944874167f6f24c87f86dab83f7b7996b314
Ashutosh Maurya <ashutoshm9695@gmail.com>
2022-11-04 08:03:20 +0530

    user schema and password setup

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
@@ -22,6 +23,20 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+//pre-save is a trigger that gets a function and execute it before user object :
+userSchema.pre("save", async (next) => {
+  const user = this;
+  const hash = await bcrypt.hash(this.password, 12);
+  this.password = hash;
+  next();
+});
+
+userSchema.method.isValidPassword = async (password) => {
+  const user = this;
+  const compare = await bcrypt.compare(password, user.password);
+  return compare;
+};
 
 const User = mongoose.model("User", userSchema);
 
